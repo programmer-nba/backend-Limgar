eeddb2628ca3ecfcf395de684cc284215c47e6e8
NBADigital <programmer@nbadigitalworlds.com>
2024-01-13 14:00:33 +0700

    update

--- routes/me.js
+++ routes/me.js
@@ -1,14 +1,14 @@
 const router = require("express").Router();
 require("dotenv").config();
-const { Admins } = require("../model/user/admin.model");
+const {Admins} = require("../model/user/admin.model");
 const auth = require("../lib/auth");
 
 router.post("/", auth, async (req, res) => {
-  const { decoded } = req;
+  const {decoded} = req;
   try {
     const id = decoded._id;
     if (decoded && decoded.row === "admin") {
-      Admins.findOne({ _id: id })
+      Admins.findOne({_id: id})
         .then((item) => {
           return res.status(200).send({
             name: item.admin_name,
@@ -18,25 +18,13 @@ router.post("/", auth, async (req, res) => {
           });
         })
         .catch(() => {
-          res.status(409).send({ message: "มีบางอย่างผิดพลาด", status: false });
+          res.status(409).send({message: "มีบางอย่างผิดพลาด", status: false});
         });
-    } else if (decoded && decoded.row === "employee") {
-      console.log("ไม่ใช่แอดมิน")
-      // Employees.findOne({ _id: id })
-      //   .then((item) => {
-      //     return res.status(200).send({
-      //       name: item.name,
-      //       username: item.username,
-      //       level: "employee",
-      //       position: item.position,
-      //     });
-      //   })
-      //   .catch(() => {
-      //     res.status(409).send({ message: "มีบางอย่างผิดพลาด", status: false });
-      //   });
+    } else {
+      console.log("Admins not found");
     }
   } catch (err) {
-    return res.status(500).send({ message: "Internal Server Error" });
+    return res.status(500).send({message: "Internal Server Error"});
   }
 });
 
