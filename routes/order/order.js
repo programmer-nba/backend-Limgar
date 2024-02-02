8ef7a7d9ab093648e7947d3676a1342cec14fff0
Rainviper7 <pongsakorn.mon@gmail.com>
2024-02-02 11:37:10 +0700

    HOTFIX unlock auth in order route

--- routes/order/order.js
+++ routes/order/order.js
@@ -3,12 +3,20 @@ const order = require("../../controllers/order/order.controller");
 const auth_admin = require("../../lib/auth.admin");
 const auth_agent = require("../../lib/auth.agent");
 
-router.post("/add", auth_agent, order.create);
-router.get("/", auth_admin, order.getOrderAll);
+//router.post("/add", auth_agent, order.create);
+//router.get("/", auth_admin, order.getOrderAll);
+//router.get("/:id", order.getOrderById);
+//router.get("/:id", order.getOrderByAgentOid);
+//router.put("/:id", auth_admin, order.update);
+//router.delete("/:id", auth_admin, order.delete);
+
+//--unlock auth 
+router.post("/add", order.create);
+router.get("/", order.getOrderAll);
 router.get("/:id", order.getOrderById);
 //router.get("/:id", order.getOrderByAgentOid);
-router.put("/:id", auth_admin, order.update);
-router.delete("/:id", auth_admin, order.delete);
+router.put("/:id", order.update);
+router.delete("/:id", order.delete);
 
 //router.put("/request/:id", order.holdOrder);//--
 //router.put("/request/:id/_:oid", order.holdOrderById);//--
