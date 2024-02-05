cae045ed1e3b44288f0837029f21715afc2f611d
Rainviper7 <pongsakorn.mon@gmail.com>
2024-02-05 09:40:30 +0700

    Hotfix - add playground for test

--- config/db.js
+++ config/db.js
@@ -7,6 +7,7 @@ module.exports = () => {
   try {
     mongoose.set("strictQuery", false);
     mongoose.connect(process.env.DB, connectionParams);
+    //mongoose.connect(process.env.DB_testter, connectionParams);
     console.log("Connected to database successfully");
   } catch (error) {
     console.log(error);
