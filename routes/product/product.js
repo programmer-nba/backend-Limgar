70923baf8019812f182f44d26fe5cc7b13ce8aa7
Programmer NBA <137274609+programmer-nba@users.noreply.github.com>
2024-02-20 15:10:33 +0700

    Revert "up"
    This reverts commit 1be41571608684bde15514b04d24e282591f7d9e.

--- routes/product/product.js
+++ routes/product/product.js
@@ -1,12 +1,11 @@
 const router = require("express").Router();
 const products = require("../../controllers/product/product.controller");
 const authAdmin = require("../../lib/auth.admin");
-const authAgent = require("../../lib/auth.agent");
-const auth = require("../../lib/auth.me")
+const authAgent = require("../../lib/auth.agent")
 
 router.post("/", authAdmin, products.create);
-router.get("/", auth, products.getProductAll);
-router.get("/:id", auth, products.getProductById);
+router.get("/", authAgent, products.getProductAll);
+router.get("/:id", authAgent, products.getProductById);
 router.put("/:id", authAdmin, products.update);
 router.delete("/:id", authAdmin, products.delete);
 
--- routes/product/product_price.js
+++ routes/product/product_price.js
@@ -1,12 +1,11 @@
 const router = require("express").Router();
 const productPrice = require("../../controllers/product/product_price.controller");
 const authAdmin = require("../../lib/auth.admin");
-const authAgent = require("../../lib/auth.agent");
-const auth = require("../../lib/auth.me");
+const authAgent = require("../../lib/auth.agent")
 
 router.post("/", authAdmin, productPrice.create);
-router.get("/", auth, productPrice.getProductPriceAll);
-router.get("/:id", auth, productPrice.getProductPriceByProduct_oid);
+router.get("/", authAgent, productPrice.getProductPriceAll);
+router.get("/:id", authAgent, productPrice.getProductPriceByProduct_oid);
 router.put("/:id", authAdmin, productPrice.update);
 router.put("/byproduct/:id", authAdmin, productPrice.updateByProduct_oid);
 router.delete("/:id", authAdmin, productPrice.delete);
