96a12012d9cf0c786678f26ec10ab87e5432d2d3
Programmer NBA <137274609+programmer-nba@users.noreply.github.com>
2024-02-20 15:06:13 +0700

    up

--- routes/product/product.js
+++ routes/product/product.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const products = require("../../controllers/product/product.controller");
 const authAdmin = require("../../lib/auth.admin");
+const authAgent = require("../../lib/auth.agent")
 
 router.post("/", authAdmin, products.create);
-router.get("/", authAdmin, products.getProductAll);
-router.get("/:id", authAdmin, products.getProductById);
+router.get("/", authAgent, products.getProductAll);
+router.get("/:id", authAgent, products.getProductById);
 router.put("/:id", authAdmin, products.update);
 router.delete("/:id", authAdmin, products.delete);
 
--- routes/product/product_price.js
+++ routes/product/product_price.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const productPrice = require("../../controllers/product/product_price.controller");
 const authAdmin = require("../../lib/auth.admin");
+const authAgent = require("../../lib/auth.agent")
 
 router.post("/", authAdmin, productPrice.create);
-router.get("/", authAdmin, productPrice.getProductPriceAll);
-router.get("/:id", authAdmin, productPrice.getProductPriceByProduct_oid);
+router.get("/", authAgent, productPrice.getProductPriceAll);
+router.get("/:id", authAgent, productPrice.getProductPriceByProduct_oid);
 router.put("/:id", authAdmin, productPrice.update);
 router.put("/byproduct/:id", authAdmin, productPrice.updateByProduct_oid);
 router.delete("/:id", authAdmin, productPrice.delete);
