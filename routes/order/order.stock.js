a1d64f875ad85d0670f98f22dd77dd16e1edf2fd
Programmer NBA <137274609+programmer-nba@users.noreply.github.com>
2024-03-04 16:45:11 +0700

    up

--- routes/order/order.stock.js
+++ routes/order/order.stock.js
@@ -3,8 +3,8 @@ const order_stock = require("../../controllers/order/order.stock.controller")
 const auth_admin = require("../../lib/auth.admin")
 const auth_employee = require("../../lib/auth.employee")
 
-router.get("/all", auth_admin, order_stock.getOrderStockAll)
-router.get("/:id", auth_employee, order_stock.getOrderStockById)
-router.get("/stock/:id", auth_employee, order_stock.getOrderStockId)
+router.get("/all", order_stock.getOrderStockAll)
+router.get("/:id", order_stock.getOrderStockById)
+router.get("/stock/:id", order_stock.getOrderStockId)
 
 module.exports = router;
